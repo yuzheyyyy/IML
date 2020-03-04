8e8bc47a4e6b86c505e7a7fb4d6e9b6769369bb7
yuzheyyyy <58148728+yuzheyyyy@users.noreply.github.com>
2020-03-04 23:23:37 +0100

    使用SVR

--- Task 1b/non_linear_regression.py
+++ Task 1b/non_linear_regression.py
@@ -1,13 +1,6 @@
 import numpy as np
 import read_data
-import math
-from sklearn import preprocessing
-from sklearn.model_selection import KFold
-from sklearn.linear_model import Ridge
-from sklearn.metrics import mean_squared_error
-from math import sqrt
-
-
+from sklearn.svm import LinearSVR
 
 #read data
 data,lable=read_data.read_data()
@@ -17,35 +10,44 @@ lable=np.array(lable)
 #non_linear
 data_nonlinear=np.hstack((data,np.square(data),np.exp(data),np.cos(data),np.ones((np.size(data,0),1))))
 
+#
+regr=LinearSVR(C=.01)
+regr.fit(data_nonlinear,lable)
+print(regr.coef_)
+
+#save data
+np.savetxt("model_coefficient.csv",regr.coef_,delimiter='\r\n')
+
 ##normalize
 #data_norm=preprocessing.scale(data_nonlinear)
+#lable_norm=preprocessing.scale(lable)
 
-#ridge regression parameter
-ridge_lambda=np.linspace(10,30,num=20)
+##ridge regression parameter
+#ridge_lambda=np.linspace(0.01,0.1,100)
+#
+##find minimum RMSE for lambda
+#min_RMSE=math.inf
+#RMSE_coef=np.zeros(21)
+#
+##RMSE setting
+#lam_loss=np.zeros(np.size(ridge_lambda))
 
-#find minimum RMSE for lambda
-min_RMSE=math.inf
-RMSE_coef=np.zeros(21)
+##cross_validation
+#for ind,lam in enumerate(ridge_lambda):
+#    regr=Lasso(lam)
+#    loss=-cross_val_score(regr,data_norm,lable,cv=10,scoring='neg_mean_squared_error')
+#    lam_loss[ind]=np.mean(np.sqrt(loss))
+#
+#lam=0.02
+#model=Lasso(lam)
+#model.fit(data_nonlinear,lable)
+#print(model.coef_)
+#print(lam_loss[np.where(ridge_lambda==lam)])
+#print(ridge_lambda)
+#
+##plot
+#plt.plot(ridge_lambda,lam_loss)
 
-#RMSE setting
-RMSE_lam=np.zeros(np.size(ridge_lambda))
 
-#cross_validation
-kf=KFold(n_splits=10)
-for index,lam in enumerate(ridge_lambda):
-    regr=Ridge(lam)
-    RMSE=[]
-    for train_inx,test_ind in kf.split(data_nonlinear):
-        regr.fit(data_nonlinear[train_inx,:],lable[train_inx])
-        rms = sqrt(mean_squared_error(lable[test_ind], regr.predict(data_nonlinear[test_ind])))
-        RMSE.append(rms)
-    RMSE_lam[index]=np.mean(np.array(RMSE))
-    #find minimum RMSE
-    if RMSE_lam[index]<min_RMSE:
-        min_RMSE=RMSE_lam[index]
-        RMSE_coef=regr.coef_
-    
-#save data
-np.savetxt("model_coefficient.csv",RMSE_coef,delimiter='\r\n')
 
-##
+

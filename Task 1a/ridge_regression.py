7f6eb3cf3958f690ca0b4ad4d70c8a23c8a0ead4
yuzheyyyy <58148728+yuzheyyyy@users.noreply.github.com>
2020-03-04 18:28:35 +0100

    修改为cross_validation的计算方法

--- Task 1a/ridge_regression.py
+++ Task 1a/ridge_regression.py
@@ -1,9 +1,8 @@
 import numpy as np
 import read_data
 from sklearn import preprocessing
-from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import Ridge
-from sklearn.metrics import mean_squared_error
 from math import sqrt
 
 
@@ -16,19 +15,12 @@ data_norm=preprocessing.scale(data)
 #ridge regression parameter
 ridge_lambda=np.array([0.01,0.1,1,10,100])
 
-#RMSE setting
-RMSE_lam=np.zeros([5])
-
 #cross_validation
-kf=KFold(n_splits=10)
+lam_loss=np.zeros(5)
 for index,lam in enumerate(ridge_lambda):
     regr=Ridge(lam)
-    RMSE=[]
-    for train_inx,test_ind in kf.split(data_norm):
-        regr.fit(data_norm[train_inx,:],lable[train_inx]) #train ridge regression model
-        rms = sqrt(mean_squared_error(lable[test_ind], regr.predict(data_norm[test_ind]))) #calculate RMSE
-        RMSE.append(rms)
-    RMSE_lam[index]=np.mean(np.array(RMSE))
+    loss=np.array(-cross_val_score(regr,data_norm,lable,cv=10,scoring='neg_mean_squared_error'))
+    RMSE=np.mean(np.sqrt(loss))
+    lam_loss[index]=RMSE
 
-#save data
-np.savetxt('RMSE.csv',RMSE_lam,delimiter='\r\n')
+np.savetxt('RMSE.csv',lam_loss,delimiter='\r\n')
